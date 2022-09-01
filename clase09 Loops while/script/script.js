d2191919292cf01de3c8739a97108307fb3d6def
Leonardo Viegas <leojviegas@hotmail.com>
2022-09-01 00:43:40 -0300

    por alguna razon era la arrow el problema, lcdll, la puse como normal y anda. ahora voy a ver si cambiandola de lugar anda igual

--- clase09 Loops while/script/script.js
+++ clase09 Loops while/script/script.js
@@ -220,7 +220,17 @@ function piedraPapelTijera() {
     let loop = true
     userScore = 0
     pcScore = 0
-
+    function evalPuntaje(a, b) {
+        if (a > b) {
+            return "Enhorabuena!"
+        }
+        if (a < b) {
+            return "Mejor suerte la próxima..."
+        }
+        if (a == b) {
+            return "Estuvo parejo, la próxima seguro ganas"
+        }
+    }
     while (loop == true) {
 
 
@@ -276,18 +286,8 @@ function piedraPapelTijera() {
         if (volverAJugar == "no") {
             loop = false
             alert("Puntaje:\n\nTú: " + userScore + "\n" + "Computadora: " + pcScore + "\n" + evalPuntaje(userScore, pcScore))
-            
-            function evalPuntaje(a, b) {
-                if (a > b) {
-                    return "Enhorabuena!"
-                }
-                if (a < b) {
-                    return "Mejor suerte la próxima..."
-                }
-                if (a == b) {
-                    return "Estuvo parejo, la próxima seguro ganas"
-                }
-            }
+
+
         }
     }
 
