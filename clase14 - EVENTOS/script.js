20afd6944e904e532728b3bb0bdc39b3ab3be627
Leonardo Viegas <leojviegas@hotmail.com>
2022-10-06 00:46:16 -0300

    a mimir

--- clase14 - EVENTOS/script.js
+++ clase14 - EVENTOS/script.js
@@ -1,3 +1,18 @@
-let boton =  document.querySelector("button")
+let boton = document.querySelector(".boton")
 
-boton.addEventListener
+boton.addEventListener("click", seEjecutaEnEvento)
+
+function seEjecutaEnEvento() {
+    let beep = document.createElement("p")
+    beep.insertAdjacentHTML("afterbegin", "BEEP")
+    let body = document.body
+    body.appendChild(beep)
+
+    let r = Math.floor((Math.random()) * 256)
+    let g = Math.floor((Math.random()) * 256)
+    let b = Math.floor((Math.random()) * 256)
+    body.style.backgroundColor = `rgb(${r}, ${g}, ${b})`
+    console.log(r, g, b)
+
+}
+
